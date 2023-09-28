a149856200fc70ca6cf2e6283d531b9c450560da
Farok-Hossain <farokafs7@gmail.comm>
2023-09-29 02:03:02 +0600

    comment added

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -3,11 +3,11 @@ import useFirebase from "../hooks/useFirebase";
 
 export const AuthContext = createContext();
 
-
 const AuthProvider = ({ children }) => {
   const allContext = useFirebase();
   return (
     <AuthContext.Provider value={allContext}>{children}</AuthContext.Provider>
+    // <AuthContext.Provider value={allContext}>{children}</AuthContext.Provider>
   );
 };
 
